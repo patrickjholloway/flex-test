0c0bd9ab8f500e09ca33567c48767740bbbb26ab
Patrick Holloway <patrick@kinvey.com>
2017-03-06 16:24:51 -0500

    mbta-ize all the things

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const sdk = require('kinvey-flex-sdk');
-
+function noop = {};
 var service = sdk.service({port:3000},(err, flex) => {
-  var dataSdk = flex.data;   // gets the datalink object from the service
-  var widgets = dataSdk.serviceObject('CoolCollection');
+  var dataSdk = flex.data;
+  var flexFunctions = flex.functions;
+  var flexAuth = flex.auth;
+  dataSdk.serviceObject('red-line-schedule');
+  dataSdk.serviceObject('green-line-schedule');
+  dataSdk.serviceObject('blue-line-schedule');
+  dataSdk.serviceObject('organge-line-schedule');
+  flexFunctions.register('getRedLineData', noop);
+  flexFunctions.register('getGreenLineData', noop);
+  flexFunctions.register('getBlueLineData', noop);
+  flexFunctions.register('getOrangeLineData', noop);
+  flexAuth.register('mbtaAuth', noop);
 });
