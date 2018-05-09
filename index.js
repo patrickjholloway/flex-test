2a8614efc233f87ba79f33aac7ba27d6d5dcc11f
Patrick Holloway <patrick@kinvey.com>
2018-05-09 16:08:56 -0400

    bump to 1.0.13

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const api_key = 'wX9NwuHnZU2ToO7GmGR9');
 const sdk = require('kinvey-flex-sdk');
 function noop() {};
-var service = sdk.service({port:3000},(err, flex) => {
+var service = sdk.service({port:3000, sharedSecret: 'cf6423804948279565d40e1e2c597cdcde0c0a33a6b761d7ebebb1f225457fa03e230ffc13cb702452952d73cd66e880ee322c341869d5c87c49'},(err, flex) => {
   var dataSdk = flex.data;
   var flexFunctions = flex.functions;
   var flexAuth = flex.auth;
@@ -10,7 +10,9 @@ var service = sdk.service({port:3000},(err, flex) => {
   dataSdk.serviceObject('green-line-schedule');
   dataSdk.serviceObject('blue-line-schedule');
   dataSdk.serviceObject('organge-line-schedule');
-  flexFunctions.register('getRedLineData', noop);
+  flexFunctions.register('getRedLineData', function (context, complete, modules) {
+    complete().setBody().next();
+  });
   flexFunctions.register('getGreenLineData', noop);
   flexFunctions.register('getBlueLineData', noop);
   flexFunctions.register('getOrangeLineData', noop);
